b1af97763a09644155e5ef0474ceb6d2159e92d7
kairarty <kairarty@gmail.com>
2021-12-11 21:34:51 +0300

    Display a list

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {Hero} from "../model/hero";
+import {HEROES} from "../mock-heroes";
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent {
+  heroes = HEROES;
+  selectedHero!: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+}
